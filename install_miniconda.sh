604b86ebfb9994527c6a14e5877d2227a8ccf8ad
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-05-29 17:02:18 -0400

    install_miniconda.sh: Get some debug info.

--- install_miniconda.sh
+++ install_miniconda.sh
@@ -3,6 +3,8 @@
 # Download and configure conda.
 wget http://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh -O miniconda.sh
 bash miniconda.sh -b -p /opt/conda
+ls /opt/conda
+ls /opt/conda/bin
 /opt/conda/bin/conda config --set always_yes yes
 source /opt/conda/bin/activate root
 
