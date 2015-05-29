d704a4a1f463d6ddf229f761d9786132e65ae04f
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-05-29 16:27:22 -0400

    install_miniconda.sh: Install and activate basic conda dependencies.

--- install_miniconda.sh
+++ install_miniconda.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+
+# Download and configure conda.
+wget http://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh -O miniconda.sh
+bash miniconda.sh -b -p $HOME/miniconda
+export PATH="$HOME/miniconda/bin:$PATH"
+conda config --set always_yes yes
+source activate root
+# Install basic conda dependencies.
+conda update conda
+conda install conda-build
+conda install binstar
