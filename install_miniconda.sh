4769086dd6eb566c578e9f3f326a069c6cf51762
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-05-29 17:05:00 -0400

    install_miniconda.sh: Install bzip2.

--- install_miniconda.sh
+++ install_miniconda.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Install bzip2.
+sudo apt-get install -y bzip2
+
 # Download and configure conda.
 wget http://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh -O miniconda.sh
 bash miniconda.sh -b -p /opt/conda
