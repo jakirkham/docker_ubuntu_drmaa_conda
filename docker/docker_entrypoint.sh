7a690383df689418df0ec165285343d4a7272151
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-05-30 14:57:46 -0400

    docker/docker_entrypoint.sh: Add code for creating an entry point.

--- docker/docker_entrypoint.sh
+++ docker/docker_entrypoint.sh
@@ -1,1 +1,20 @@
 #!/bin/bash
+
+
+set -e
+
+source /usr/share/gridengine/default/common/settings.sh
+
+# Set the ulimits for this container. Must be run with the --privileged option
+#ulimit -l unlimited
+#ulimit -s unlimited
+
+echo "$HOSTNAME" >  /usr/share/gridengine/default/common/act_qmaster
+echo "domain $HOSTNAME" >> /etc/resolv.conf
+/etc/init.d/sgemaster start
+qconf -mattr "queue" "hostlist" "$HOSTNAME" "debug"
+qconf -as $HOSTNAME
+
+
+# Run whatever the user wants to
+exec "$@"
