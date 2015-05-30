7fcf672d2dc5e970d513cec0dec0273c92f418c1
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-05-30 15:26:27 -0400

    gridengine/install_ge.sh: Run `apt-get clean` as the last step.

--- gridengine/install_ge.sh
+++ gridengine/install_ge.sh
@@ -30,3 +30,5 @@ echo "Printing queue info to verify that things are working correctly."
 qstat -f -q all.q -explain a
 echo "You should see sge_execd and sge_qmaster running below:"
 ps aux | grep "sge"
+# Clean apt-get so we don't have a bunch of junk left over from our build.
+apt-get clean
