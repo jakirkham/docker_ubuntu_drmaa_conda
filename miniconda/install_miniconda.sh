94da3bf360142081385d22ebe70ba8f7e79f49c3
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-05-30 15:32:28 -0400

    miniconda/install_miniconda.sh: Activate root before updating and installing.

--- miniconda/install_miniconda.sh
+++ miniconda/install_miniconda.sh
@@ -14,9 +14,9 @@ cd /usr/share/miniconda
 wget http://repo.continuum.io/miniconda/Miniconda-3.9.1-Linux-x86_64.sh -O miniconda.sh
 bash miniconda.sh -b -p /opt/conda
 /opt/conda/bin/conda config --set always_yes yes
-source /opt/conda/bin/activate root
 
 # Install basic conda dependencies.
+source /opt/conda/bin/activate root
 /opt/conda/bin/conda update conda
 /opt/conda/bin/conda install conda-build
 /opt/conda/bin/conda install binstar
