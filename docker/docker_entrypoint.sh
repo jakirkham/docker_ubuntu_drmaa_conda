d3a5173ac3604f6137d2cac4480b5fbf241a828c
John Kirkham <kirkhamj@janelia.hhmi.org>
2015-05-31 10:38:56 -0400

    docker/docker_entrypoint.sh: Use `sudo` for all `qconf` commands.

--- docker/docker_entrypoint.sh
+++ docker/docker_entrypoint.sh
@@ -11,8 +11,8 @@ sudo sh -c 'echo "$HOSTNAME" > /var/lib/gridengine/default/common/act_qmaster'
 sudo sh -c 'echo "domain $HOSTNAME" >> /etc/resolv.conf'
 sudo service gridengine-master restart
 sudo service gridengine-exec restart
-qconf -mattr "queue" "hostlist" "$HOSTNAME" "debug"
-qconf -as $HOSTNAME
+sudo qconf -mattr "queue" "hostlist" "$HOSTNAME" "debug"
+sudo qconf -as $HOSTNAME
 
 
 # Run whatever the user wants to
